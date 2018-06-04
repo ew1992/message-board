92b7381a475bf29b3bf8caee576d8bbd9e71fa6c
eighteen <ew1992ew@yahoo.co.jp>
2018-06-04 14:39:56 +0000

    add kaminari

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,8 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @messages = Message.all
+    @messages = Message.order(created_at: :desc).page(params[:page]).per(5)
   end
   
   def show
